fa889eaf115c214e62d4f8ff878af337e30d4c08
rochafialvin <rochafi.dev@gmail.com>
2020-05-15 09:35:34 +0700

    card columns

--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
 
 function ImageList(props) {
+
+   // props.images = [{image}, {image}, {image}]
+   // image : { id, desription, alt_description, urls, likes, user }
+   const images = props.images.map(image => {
+      return (
+         <div class="card">
+            <img class="card-img-top" src={image.urls.regular} alt={image.description} /> {/* self closing tag */}
+            <div class="card-body">
+            <h5 class="card-title">{image.description}</h5>
+               <p class="card-text">{image.alt_description}</p>
+            </div>
+         </div>
+      )
+   })
+
    return(
-      <div>
-         <h1>Terdapat {props.images.length} gambar</h1>
+      <div className="card-columns" >
+         {images}
       </div>
    )
 }
 
 export default ImageList;
+
+/*
+   image : {
+      id, desription, alt_description, urls, likes, user
+   }
+*/
